c08076d278d62c2b4abccde7705e273e05f419ef
gabys0979 <gaby Garcia>
2023-05-16 18:45:52 -0500

    final

--- PyBank/profits.py
+++ PyBank/profits.py
@@ -19,22 +19,23 @@ with open(csvpath, encoding='utf') as csvfile:
     budgetData = csv.reader(csvfile, delimiter=",")
     #skip header
     next(budgetData)
-    #iterate through rows calculating changes
+    # calculating changes
     for row in budgetData:
-        totalMonths = totalMonths + 1 #increase month count
-        netprofit += int(row[1]) #add to profit count
-        change = int(row[1])-prevmonth #monthly difference
-        #if profit increased check if it set a new record
+        totalMonths = totalMonths + 1 
+        netprofit += int(row[1]) 
+        change = int(row[1])-prevmonth #difference
+        #check if it set a new record
         if change > 0:
             if change > int(greatestIncrease[1]):
                 greatestIncrease = [row[0], str(change)]
-        #if profit decreased check if it is a new record
+        #profit decreased check if it is a new record
         if change < 0: 
             if change < int(greatestDecrease[1]):
                 greatestDecrease = [row[0], str(change)]
-        monthlyChanges += change #add to cumulative changes
-        prevmonth = int(row[1]) #set current month as new previous month, then move to next month
-    #output results to terminal'
+        monthlyChanges += change #cumulative changes
+        prevmonth = int(row[1]) 
+
+    #output results to terminal
     print("Financial Analysis")
     print("----------------------------")
     print(f"Total Months: {totalMonths}")
@@ -44,7 +45,6 @@ with open(csvpath, encoding='utf') as csvfile:
     print(f"Greatest Decrease in Profits: {greatestDecrease[0]} (${greatestDecrease[1]})")
 
 #write to a text file
-# The following code benefited from: https://stackoverflow.com/questions/36571560/directing-print-output-to-a-txt-file
 with open("analysis\profits.txt", 'w') as file:
     print("Financial Analysis", file = file)
     print("----------------------------", file = file)
