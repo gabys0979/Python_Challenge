4381fef9dfb37ac502704646de7fb58c08909e23
gabys0979 <gaby Garcia>
2023-06-15 19:26:50 -0500

    final

--- PyPoll/election_results.py
+++ PyPoll/election_results.py
@@ -2,24 +2,24 @@
 import os
 import csv
 
-#file path 
+#define file path relative to current working directory
 dataPath = os.path.join("Resources", "election_data.csv")
 
 #create variables
 totalVotes = 0 #count of total votes cast
-candidates = {} #dictionary candidates
+candidates = {} #dictionary for  results of candidates
 
-#open the file using "read" mode 
+#open the file using "read" mode and assign to a variable
 with open(dataPath, 'r', encoding='utf') as csvfile:
-    pollData = csv.reader(csvfile, delimiter=",") 
+    pollData = csv.reader(csvfile, delimiter=",") #read the file
     next(pollData) #skip the header
-    for i in pollData: 
+    for i in pollData: #iterate through all rows containing data
         if i[2] not in candidates: #check if candidate voted for is in dictionary if not:
-            candidates[str(i[2])] = [1, 0] #add  = candidate name and values = vote count and percentage of votes
-        else: #if candidate is in dictionary  add a vote to their tally
+            candidates[str(i[2])] = [1, 0] #add an entry with key = candidate name and values = vote count and percentage of total votes
+        else: #if candidate is already in dictionary then add a vote to their tally
             candidates[str(i[2])][0] += 1
-        totalVotes += 1 
-    for i in candidates: 
+        totalVotes += 1 #add a vote to to the total vote count
+    for i in candidates: #iterate through candidates to calculate their percentage of total vote
         candidates[f'{i}'][1] = candidates[f'{i}'][0]/totalVotes
 
 
@@ -31,7 +31,6 @@ print("-------------------------")
 for i in candidates:
     print(f"{i}: {round(candidates[f'{i}'][1]*100,3)}% ({candidates[f'{i}'][1]})")
 print("-------------------------")
-
 #compare to see the winner
 winner = 0
 for i in candidates:
